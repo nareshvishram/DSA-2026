6ca91de275c113b9bff22063b8a37a69d8f8b1b0
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-04-22 21:22:20 +0530

    Time: 6 ms (97.26%), Space: 47.6 MB (77.46%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -14,6 +14,25 @@ class Solution {
             return root;
         if (root.val == p.val || root.val == q.val)
             return root;
+        TreeNode left = null, right = null;
+        if (root.val < p.val && root.val < q.val)
+            right = lowestCommonAncestor(root.right, p, q);
+        else if (root.val > p.val && root.val > q.val)
+            left = lowestCommonAncestor(root.left, p, q);
+        else if ((root.val > p.val && root.val < q.val) || (root.val < p.val && root.val > q.val))
+            return root;
+        if (left != null && right != null)
+            return root;
+
+        return left != null ? left : right;
+
+    }
+
+    public TreeNode lowestCommonAncestorNormal(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return root;
+        if (root.val == p.val || root.val == q.val)
+            return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         if (left == null && right != null)
