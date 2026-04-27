c0c25a90a1f5a6747e04a7d40f322c1924ab69af
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-04-27 21:19:34 +0530

    Time: 15 ms (42.16%), Space: 49.3 MB (40.64%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    int[][] mat;
+    int[][] dist;
+    int[][] dir = new int[][] { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+
+    public int[][] updateMatrix(int[][] matrix) {
+        this.mat = matrix;
+        dist = new int[matrix.length][matrix[0].length];
+        multiSourceBfs();
+        return dist;
+    }
+
+    private void multiSourceBfs() {
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    q.offer(new int[] { i, j });
+                } else {
+                    dist[i][j] = -1;
+                }
+            }
+        }
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int x = curr[0], y = curr[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dir[i][0];
+                int ny = y + dir[i][1];
+                if (nx >= 0 && ny >= 0 && nx < mat.length && ny < mat[0].length && dist[nx][ny] == -1) {
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.offer(new int[]{nx,ny});
+                }
+            }
+        }
+    }
+}
