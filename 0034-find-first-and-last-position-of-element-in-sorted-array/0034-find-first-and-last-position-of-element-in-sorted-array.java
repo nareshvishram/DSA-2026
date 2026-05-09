b85a222e42869a419b3adb7310c28afdee5b5f3f
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-05-09 13:01:22 +0530

    Time: 0 ms (100%), Space: 47.9 MB (86.04%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,38 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l = 0, r = nums.length - 1, mid = -1, n = nums.length;
-        boolean flag = false;
-        while (l <= r) {
-            mid = (r - l) / 2 + l;
-            if (nums[mid] == target) {
-                flag = true;
-                break;
-            }
-            if (nums[mid] > target)
-                r = mid - 1;
-            else
-                l = mid + 1;
-        }
-        int[] ans = new int[] { -1, -1 };
-        if (!flag)
-            return ans;
-        // left
-        // right
-        ans[0] = mid;
-        ans[1] = mid;
-        if (mid == 0) {
-            while (mid < n && nums[mid] == target)
-                mid++;
-            ans[1] = mid - 1;
-            return ans;
-        }
-        if (mid == n - 1) {
-            while (mid >= 0 && nums[mid] == target)
-                mid--;
-            ans[0] = mid + 1;
-            return ans;
-        }
-        int tmp = mid;
-        if (nums[tmp - 1] == target) {
-            // mid to 0
-            while (tmp >= 0 && nums[tmp] == target)
-                tmp--;
-            ans[0] = tmp + 1;
-        }
-        tmp = mid;
-        if (nums[tmp + 1] == target) {
-            // mid to n-1;
-            while (tmp < n && nums[tmp] == target)
-                tmp++;
-            ans[1] = tmp - 1;
-        }
-        return ans;
+        int first = bs1(nums, target, 0, nums.length - 1);
+        int last = bs2(nums, target, 0, nums.length - 1);
+        return new int[] { first, last };
+    }
 
+    private int bs1(int[] nums, int target, int l, int r) {
+        int idx = -1;
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if (nums[mid] >= target)
+                r = mid - 1;
+            else {
+                l = mid + 1;
+            }
+            if (nums[mid] == target)
+                idx = mid;
+
+        }
+        return idx;
+    }
+
+    private int bs2(int[] nums, int target, int l, int r) {
+        int idx = -1;
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if (nums[mid] <= target)
+                l = mid + 1;
+            else {
+                r = mid - 1;
+            }
+            if (nums[mid] == target)
+                idx = mid;
+        }
+        return idx;
     }
 }
