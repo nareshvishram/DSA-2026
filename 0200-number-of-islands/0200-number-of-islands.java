de83cabfd98586cc0d1783d38240fa08d9915138
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-06-19 01:52:35 +0530

    Time: 4 ms (45.26%), Space: 52.2 MB (59.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,36 @@
 class Solution {
+    int m, n;
+    int dir[][] = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+
     public int numIslands(char[][] grid) {
-        int ans = 0;
-        boolean vis[][] = new boolean[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        m = grid.length;
+        n = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    ans++;
+                    count++;
+                    dfs(i, j, grid);
                 }
             }
         }
-        return ans;
+        return count;
+
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-        if (!isValid(grid, i, j))
+    private void dfs(int i, int j, char[][] grid) {
+        if (!isValid(i, j, grid))
             return;
         grid[i][j] = '0';
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j - 1);
+        for (int k = 0; k < 4; k++) {
+            if (isValid(i + dir[k][0], j + dir[k][1], grid))
+                dfs(i + dir[k][0], j + dir[k][1], grid);
+        }
     }
 
-    private boolean isValid(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
-            return false;
-        return true;
+    private boolean isValid(int i, int j, char[][] grid) {
+        if (i >= 0 && j >= 0 && i < m && j < n && grid[i][j] == '1')
+            return true;
+        return false;
     }
 }
