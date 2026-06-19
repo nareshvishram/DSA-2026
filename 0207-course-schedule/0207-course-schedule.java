2fe3a99d17b388280e44c7e0d26265a1f69a8ec6
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-06-20 01:15:24 +0530

    Time: 9 ms (19.03%), Space: 46.7 MB (92.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,29 +1,27 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] preq) {
+    public boolean canFinish(int courses, int[][] pre) {
+        int[] indeg = new int[courses];
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        int indeg[] = new int[numCourses];
-        if (preq.length == 0)
-            return true;
-        for (int i = 0; i < numCourses; i++)
+        for (int i = 0; i < courses; i++)
             graph.put(i, new ArrayList<>());
-        for (int x[] : preq) {
-            graph.get(x[0]).add(x[1]);
+        for (int[] x : pre) {
             indeg[x[1]]++;
+            graph.get(x[0]).add(x[1]);
         }
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < indeg.length; i++) {
+        for (int i = 0; i < courses; i++) {
             if (indeg[i] == 0)
                 q.add(i);
         }
-        int v = 1;
+        if (q.size() == 0)
+            return false;
         while (!q.isEmpty()) {
             int curr = q.poll();
-            System.out.println(curr);
             for (int child : graph.get(curr)) {
                 indeg[child]--;
-                if (indeg[child] == 0)
+                if (indeg[child] == 0) {
                     q.add(child);
-                v++;
+                }
             }
         }
         for (int x : indeg)
@@ -32,8 +30,5 @@ class Solution {
         if (!q.isEmpty())
             return false;
         return true;
-
     }
-
-    
 }
