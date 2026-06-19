aeb65834e25ad478b0d69b3f188a9bcbb9d2f3e5
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-06-20 02:15:33 +0530

    Time: 1 ms (37.88%), Space: 46.9 MB (42.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,27 @@
 class Solution {
-    boolean vis[][];
+    int m, n, fillColor, orgColor;
+    int[][] direx = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        vis = new boolean[image.length][image[0].length];
-        dfs(image, sr, sc, color, image[sr][sc]);
+        m = image.length;
+        n = image[0].length;
+        fillColor = color;
+        orgColor = image[sr][sc];
+        dfs(image, sr, sc);
         return image;
     }
 
-    private void dfs(int[][] image, int i, int j, int color, int srcColor) {
-        if (i < 0 || j < 0 || i >= image.length || j >= image[0].length || image[i][j] != srcColor || vis[i][j] == true)
-            return;
-        image[i][j] = color;
-        vis[i][j] = true;
-        dfs(image, i + 1, j, color, srcColor);
-        dfs(image, i - 1, j, color, srcColor);
-        dfs(image, i, j + 1, color, srcColor);
-        dfs(image, i, j - 1, color, srcColor);
+    private void dfs(int[][] image, int i, int j) {
+        image[i][j] = fillColor;
+        for (int k = 0; k < 4; k++) {
+            if (isValid(direx[k][0] + i, direx[k][1] + j, image))
+                dfs(image, direx[k][0] + i, direx[k][1] + j);
+        }
+    }
+
+    private boolean isValid(int i, int j, int[][] image) {
+        if (i >= 0 && j >= 0 && i < m && j < n && image[i][j] == orgColor && image[i][j] != fillColor)
+            return true;
+        return false;
     }
 }
