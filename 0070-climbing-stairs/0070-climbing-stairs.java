df179e0294090be6d7035b433036b6407875dd92
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-06-20 19:32:31 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.9%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 0)
-            return 0;
+        int dp[] = new int[n + 1];
         if (n == 1 || n == 2)
             return n;
-        int dp[] = new int[n + 1];
-        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i <= n; i++)
             dp[i] = dp[i - 1] + dp[i - 2];
         return dp[n];
-
-    }
-
-    public int recursive(int n) {
-        if (n <= 0)
-            return 0;
-        if (n == 1 || n == 2)
-            return n;
-        return recursive(n - 1) + recursive(n - 2);
 
     }
 }
