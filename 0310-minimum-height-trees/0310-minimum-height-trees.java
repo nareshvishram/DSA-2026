7dd08d4c5048c1e4fb4664de71a6a3973294484d
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-06-20 13:36:31 +0530

    Time: 22 ms (59.74%), Space: 71.3 MB (37.61%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,41 +1,38 @@
 class Solution {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
-
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if(n==1)
+        if (n == 1)
             return Arrays.asList(0);
+        Map<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < n; i++)
-            graph.put(i, new ArrayList<>());
-        int[] deg = new int[n];
-        for (int[] x : edges) {
-            graph.get(x[0]).add(x[1]);
-            graph.get(x[1]).add(x[0]);
-            deg[x[0]]++;
-            deg[x[1]]++;
-        }
+            map.put(i, new ArrayList<>());
+        int[] indeg = new int[n];
         Queue<Integer> q = new LinkedList<>();
+        for (int[] e : edges) {
+            map.get(e[0]).add(e[1]);
+            map.get(e[1]).add(e[0]);
+            indeg[e[0]]++;
+            indeg[e[1]]++;
+        }
         for (int i = 0; i < n; i++) {
-            if (deg[i] == 1)
+            if (indeg[i] == 1)
                 q.add(i);
         }
-
-        List<Integer> ans = new ArrayList<>();
-        int count = n;
-        while (!q.isEmpty() && count > 2) {
+        int remaining = n;
+        while (remaining > 2) {
             int size = q.size();
             while (size-- > 0) {
-                int curr = q.poll();
-                deg[curr]--;
-                count--;
-                for (int child : graph.get(curr)) {
-                    deg[child]--;
-                    if (deg[child] == 1)
+                int parent = q.poll();
+                remaining--;
+                for (int child : map.get(parent)) {
+                    indeg[child]--;
+                    if (indeg[child] == 1)
                         q.add(child);
+
                 }
             }
         }
-        while (!q.isEmpty())
-            ans.add(q.poll());
-        return ans;
+        //List<Integer> ans = new ArrayList<>();
+        return new ArrayList<>(q);
+
     }
 }
