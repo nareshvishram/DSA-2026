ff322cca20e309ddf9d3c2c4057284f392ac6a77
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-06-21 12:08:14 +0530

    Time: 0 ms (100%), Space: 43.5 MB (45.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,35 +2,35 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         int[][] dp = new int[m][n];
-        boolean obstacle = false;
+        boolean isObst = false;
+        for (int i = 0; i < n; i++) {
+            if (grid[0][i] == 1)
+                isObst = true;
+            if (isObst)
+                dp[0][i] = 0;
+            else
+                dp[0][i] = 1;
+        }
+        isObst = false;
         for (int i = 0; i < m; i++) {
-            if (grid[i][0] == 1) {
-                obstacle = true;
-            }
-            if (obstacle)
+            if (grid[i][0] == 1)
+                isObst = true;
+            if (isObst)
                 dp[i][0] = 0;
             else
                 dp[i][0] = 1;
         }
-        obstacle = false;
-        for (int i = 0; i < n; i++) {
-            if (grid[0][i] == 1) {
-                obstacle = true;
-            }
-            if (obstacle)
-                grid[0][i] = 0;
-            else
-                dp[0][i] = 1;
-        }
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (grid[i][j] == 1)
-                    dp[i][j] = 0;
-                else
+                if (grid[i][j] != 1)
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                else
+                    dp[i][j] = 0;
             }
         }
+        // for (int[] x : dp)
+        //     System.out.println(Arrays.toString(x));
         return dp[m - 1][n - 1];
 
     }
