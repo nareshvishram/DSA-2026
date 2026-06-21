6416bdbdc34db6304bf9ccc2930e9e1aa683ef60
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2026-06-21 20:46:25 +0530

    Time: 27 ms (10.24%), Space: 98.2 MB (27.98%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[] arr;
+    List<List<Integer>> ans;
+
+    public List<List<Integer>> combine(int n, int k) {
+        ans = new ArrayList<>();
+        arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = i + 1;
+        bt(n, k, 0, new ArrayList<>());
+        return ans;
+    }
+
+    private void bt(int n, int k, int start, List<Integer> tmp) {
+        if (tmp.size() == k)
+            ans.add(new ArrayList<>(tmp));
+        for (int i = start; i < n; i++) {
+            tmp.add(arr[i]);
+            bt(n, k, i + 1, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
